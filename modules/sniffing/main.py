6429addc24dad9996eed4e5d9211ac5bf2ac29c9
nonroot <NoahTroy@users.noreply.github.com>
2020-09-19 12:56:36 -0400

    Added initializer

--- modules/sniffing/main.py
+++ modules/sniffing/main.py
@@ -2,7 +2,12 @@ import pyshark
 
 
 def wifi():
-	
+	def processPacket(packet):
+		pass
+
+	capture = pyshark.LiveCapture(interface = 'eth0')
+	capture.sniff()
+	capture.apply_on_packets(processPacket)
 
 
 def bluetooth():
