b8a606b9636dd3b36235b14e44d2836a5f91f23b
Nick Dawson <nmd2117@rit.edu>
2020-09-30 10:49:00 -0400

    Created NetworkUser.py class - A class to represent a user that has been detected on a network.

---
+++ classes/tracker/NetworkUser.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""
+A class to represent a user that has been detected on a network.
+"""
+
+from time import perf_counter
+from typing import Tuple
+
+__author__ = 'Nick Dawson'
+__copyright__ = 'Copyright 2020, RITSEC'
+__license__ = 'MIT'
+__version__ = '0.1.0'
+__status__ = 'Development'
+
+
+class NetworkUser:
+    """
+    A class to represent a user that has been detected on a network.
+    """
+    def __init__(self, mac_address: str, signal_strength: float):
+        """
+        Initialize the user and store the start time to later compute the
+        total time the user was visible.
+        :param str mac_address: MAC Address ex. '00:0a:95:9d:68:16'
+        :param float signal_strength: represent network signal strength ex. -70.653
+        """
+        self.start_time = perf_counter()
+        self.mac_address = mac_address.lower()
+        self.signal_strengths = []
+        self.signal_strengths.append(signal_strength)
+
+    def update(self, signal_strength: float) -> None:
+        """
+        Add another signal strength data point to the list.
+        :param float signal_strength: float to represent network signal strength ex. -70.653
+        """
+        self.signal_strengths.append(signal_strength)
+
+    def done(self) -> Tuple[str, float, float, float, float]:
+        """
+        Run calculations to compute total time visible on the network and,
+        the average signal strength.
+        :return: Tuple in the format of:
+            (MAC address, total time, average signal strength, min signal strength, max signal strength)
+        """
+        total_time = perf_counter() - self.start_time
+        avg_signal_strength = sum(self.signal_strengths) / len(self.signal_strengths)
+        return (self.mac_address,
+                total_time,
+                avg_signal_strength,
+                min(self.signal_strengths),
+                max(self.signal_strengths))
