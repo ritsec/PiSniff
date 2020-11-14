e87511d8a92ae195e7144cae38303752a313a3ef
Noah Troy <NoahTroy@users.noreply.github.com>
2020-11-14 13:55:44 -0500

    Update main.py

--- modules/sniffing/main.py
+++ modules/sniffing/main.py
@@ -1,11 +1,16 @@
+# sudo apt install python3-pip tshark libxslt-dev python3-scapy bluez
+# sudo pip3 install pyshark scapy
+# https://scapy.readthedocs.io/en/latest/layers/bluetooth.html#opening-a-hci-socket
+
 import pyshark
+from scapy.layers.bluetooth import *
 
 
 def wifi():
 	def processPacket(packet):
 		pass
 
-	capture = pyshark.LiveCapture(interface = 'eth0')
+	capture = pyshark.LiveCapture(interface = 'mon0')
 	capture.sniff()
 	capture.apply_on_packets(processPacket)
 
